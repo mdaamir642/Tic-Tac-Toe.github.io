9fe3446f09b04e957daeed82ed7f85e774b97c17
MD AAMIR <mdaamir9110641@gmail.com>
2023-06-11 03:55:15 -0700

    Tic-TacToe App

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 
 var boxes = document.getElementsByClassName("box");
 var counter = 1;
-var currentPlayer = "";
-var player1 = "";
-var player2 = "";
-
-
-function toss(button){
+var player1 = document.getElementById("player1");
+var player2 = document.getElementById("player2");
+var tossPage = document.getElementById("tossPage")
+var currentPlayer = document.getElementById("currentPlayer")
+var wrapper = document.getElementById("wrapper")
+var tossResult = document.getElementById("tossWin")
 var result = Math.floor(Math.random()*2);
-if(result === 0){
-currentPlayer = "player1"
-player1 = `<i class="fa-regular fa-circle" id="zero"></i>`
+var startBtn = document.getElementById("startBtn")
+function toss(){
+   result = Math.floor(Math.random()*2);
+
+  if(result === 0){
+
+  tossResult.innerHTML = player1.value.toUpperCase() + " " + "WIN";
+}
+else{
+  tossResult.innerHTML = player2.value.toUpperCase() + " " + "WIN";
+  
+  currentPlayer.innerHTML = player2.value.toUpperCase() + " " + "TURN";
+
+
 
 }
 
+startBtn.style.display = "block"
 }
 
 
+function start(){
+ if(result === 0){
+  currentPlayer.innerHTML = player1.value.toUpperCase() + " " + "TURN";
+}
+ else{
+  currentPlayer.innerHTML = player2.value.toUpperCase() + " " + "TURN";
 
+ } 
+  tossPage.style.display = "none"
+
+  wrapper.style.display = "flex"
+
+}
 
 
 
@@ -37,18 +61,25 @@ function special (element){
     counter++
 if (!element.innerHTML) {
     if (counter % 2 === 0) {
+   
+       
         element.innerHTML = `<i class="fa-regular fa-circle" id="zero"></i>` 
- 
+
+      
+
     }
     
     else {
         element.innerHTML = `<i class="fa-solid fa-xmark" id="cross"></i>`
-    }
+
+   
+      }
 
  
 }
 
 checkWin();
+
 }
 
 
@@ -94,7 +125,16 @@ special(element)
          boxes[a].style.backgroundColor = "green";
          boxes[b].style.backgroundColor = "green";
          boxes[c].style.backgroundColor = "green";
-   
+   if(boxes[a, b, c] === `<i class="fa-regular fa-circle" id="zero"></i>`){
+    currentPlayer.innerHTML = player1.value.toUpperCase() + " " + "WIN";
+
+
+   }
+   else{
+    currentPlayer.innerHTML = player2.value.toUpperCase() + " " + "WIN";
+
+
+   }
          disableClicks()
          return;
 
